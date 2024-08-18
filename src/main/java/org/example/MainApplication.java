5a21ae8e7141e6b97e319ad1a11c65a068aa12ae
armin <armynm005@gmail.com>
2024-08-18 14:59:43 +0330

    chore: init

---
+++ src/main/java/org/example/MainApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class MainApplication {
+    public void printMenu() {
+        System.out.println(
+                "1. Display rooms\n" +
+                "2. Admin panel\n" +
+                "3. Exit\n\n" +
+                "Select an option: "
+        );
+    }
+}
