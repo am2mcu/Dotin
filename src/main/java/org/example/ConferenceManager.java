4a2ddf9e9286d423564ea70247efee48611acab1
armin <armynm005@gmail.com>
2024-08-20 23:55:45 +0330

    feat: add createAccount function

--- src/main/java/org/example/ConferenceManager.java
+++ src/main/java/org/example/ConferenceManager.java
@@ -1,11 +1,11 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 public class ConferenceManager {
     private static final ConferenceManager SINGLETON = new ConferenceManager();
     private final RoomHandler roomHandler = RoomHandler.getSINGLETON();
+    private final CustomerHandler customerHandler = CustomerHandler.getSINGLETON();
 
     private ConferenceManager() {
     }
@@ -18,7 +18,12 @@ public class ConferenceManager {
         return roomHandler.getRooms();
     }
 
+    // TODO: Admin interface - place in AdminPanel
     public void addRoom(int id, double price, int capacity) {
         roomHandler.addRoom(new Room(id, price, capacity));
+    }
+
+    public void createAccount(String id, String firstName, String lastName, String phoneNumber, String email) {
+        customerHandler.addCustomer(new Customer(id, firstName, lastName, phoneNumber, email));
     }
 }
