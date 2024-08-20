b5043fc97a3077a63aa1617716e6b7b934a644bc
armin <armynm005@gmail.com>
2024-08-20 23:41:21 +0330

    feat: add customer & customerHandler

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class Customer {
+    private String id;
+    private String firstName, lastName;
+    private String phoneNumber;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/org/example/CustomerHandler.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomerHandler {
+    private static final CustomerHandler SINGLETON = new CustomerHandler();
+    private final Map<String, Customer> customerMap = new HashMap<>();
+
+    private CustomerHandler() {
+    }
+
+    public static CustomerHandler getSINGLETON() {
+        return SINGLETON;
+    }
+
+    public Collection<Customer> getCustomers() {
+        return customerMap.values();
+    }
+
+    public void addPerson(Customer customer) {
+        customerMap.put(customer.getId(), customer);
+    }
+}
