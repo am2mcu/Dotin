e1316cbceae7decf54643c720056183d1cd6e58c
MohammadRamzi <mohammad131382@gmail.com>
2024-08-24 16:22:47 +0330

    fix(Application): fix 3-tier

--- src/main/java/mohammad/application/controller/Application.java
+++ src/main/java/mohammad/application/controller/Application.java
@@ -4,36 +4,40 @@ import mohammad.application.rule.CustomerHandler;
 import mohammad.application.rule.ReserveManagerHandler;
 import mohammad.application.rule.Rule;
 import mohammad.application.rule.SystemManagerHandler;
+import mohammad.data.storage.Base;
+import mohammad.data.storage.MemoryBase;
 import mohammad.presentation.InputManager;
+import mohammad.presentation.Message;
 import mohammad.presentation.Panel;
 
 public class Application {
 
-    Panel panel;
+    boolean notEOF;
     Rule rule;
+    Base base;
+    Panel panel;
+    Message message;
     InputManager inputManager;
 
 
     public Application() {
-        try {
-            lunchProgram();
-        } catch (Exception e) {
-            showExceptionMessage();
-        } finally {
-            closeProgram();
-        }
+//        try {
+        lunchProgram();
+//        } catch (Exception e) {
+//            showExceptionMessage();
+//        } finally {
+//            closeProgram();
+//        }
     }
 
 
     private void lunchProgram() {
         initializeInstances();
-        showWelcomePanel();
-        selectRule();
-        executeRule();
+        executeApplication();
     }
 
     private void showExceptionMessage() {
-        System.out.println("Some exception occurred!");
+        message.showExceptionMessage();
     }
 
     private void closeProgram() {
@@ -44,28 +48,42 @@ public class Application {
     private void initializeInstances() {
         panel = new Panel();
         inputManager = new InputManager();
+        message = new Message();
+        base = new MemoryBase(); // TODO BaseFactory
+        notEOF = true;
     }
 
-    private void showWelcomePanel() {
-        System.out.println(panel.getWelcomePanel());
+    private void executeApplication() {
+        do {
+            runApplication();
+        } while (notEOF);
     }
+
+
+    private void runApplication() {
+        selectRule();
+        executeRule();
+    }
+
 
     private void selectRule() {
+        message.showWelcomePanel();
         switch (inputManager.getChoiceInput()) {
-            case 1 -> rule = new CustomerHandler();
-            case 2 -> rule = new SystemManagerHandler();
-            case 3 -> rule = new ReserveManagerHandler();
-            default -> showInvalidRuleMessage();
+            case 1 -> rule = new CustomerHandler(base);
+            case 2 -> rule = new SystemManagerHandler(base);
+            case 3 -> rule = new ReserveManagerHandler(base);
+            case 4 -> doEOF();
+            default -> message.showInvalidRuleMessage();
         }
+    }
+
+    private void doEOF() {
+        message.showQuitMessage();
+        closeProgram();
     }
 
     private void executeRule() {
         rule.execute();
-    }
-
-
-    private void showInvalidRuleMessage() {
-        System.out.println("Invalid rule selected!");
     }
 
 }
