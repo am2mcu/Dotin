af3028303edfbf67777eb979f0cd24cdd9c469bf
armin <armynm005@gmail.com>
2024-09-07 23:59:56 +0330

    feat: add reservation model

---
+++ src/main/java/com/example/demo/model/Reservation.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+    @Id
+    @SequenceGenerator(
+            name = "reservation_sequence",
+            sequenceName = "reservation_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "reservation_sequence"
+    )
+    private Long reservationId;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Reservation(Room room, User user) {
+        this.room = room;
+        this.user = user;
+    }
+}
