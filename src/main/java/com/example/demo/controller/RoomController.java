20b878fade43b7eed6f86d57f0c8e49cc4b836c3
armin <armynm005@gmail.com>
2024-09-18 18:12:29 +0330

    feat: add swagger @Operation to api functions

--- src/main/java/com/example/demo/controller/RoomController.java
+++ src/main/java/com/example/demo/controller/RoomController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.model.Reservation;
 import com.example.demo.model.Room;
 import com.example.demo.service.RoomService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,21 +20,43 @@ public class RoomController {
         this.roomService = roomService;
     }
 
+    @Tag(name = "room", description = "Access & modify rooms")
+    @Operation(
+            summary = "Get rooms",
+            description = "Get all rooms properties",
+            tags = {"room"}
+    )
     @GetMapping("/rooms")
     public List<Room> getRooms() {
         return roomService.getRooms();
     }
 
+    @Operation(
+            summary = "Add room",
+            description = "Add a new room",
+            tags = {"room"}
+    )
     @PostMapping("/rooms")
     public void addRoom(@RequestBody Room room) {
         roomService.addRoom(room);
     }
 
+    @Operation(
+            summary = "Delete room",
+            description = "Delete an existing room",
+            tags = {"room"}
+    )
     @DeleteMapping("/rooms/{roomId}")
     public void deleteRoom(@PathVariable("roomId") Long roomId) {
         roomService.deleteRoom(roomId);
     }
 
+
+    @Operation(
+            summary = "Modify room",
+            description = "Update a room properties",
+            tags = {"room"}
+    )
     @PutMapping("/rooms/{roomId}")
     public void updateRoom(
             @PathVariable("roomId") Long roomId,
@@ -40,11 +64,23 @@ public class RoomController {
         roomService.updateRoom(roomId, capacity);
     }
 
+    @Tag(name = "reservation", description = "List & Modify reservations")
+    @Operation(
+            summary = "Get reservations",
+            description = "Get all reserved rooms properties",
+            tags = {"reservation"}
+    )
     @GetMapping("/reservations")
     public List<Reservation> getReservedRooms() {
         return roomService.getReservedRooms();
     }
 
+    @Tag(name = "reservation", description = "List & Modify reservations")
+    @Operation(
+            summary = "Reserve room",
+            description = "Reserve a new room",
+            tags = {"reservation"}
+    )
     @PostMapping("/reservations/{roomId}")
     public void reserveRoom(@PathVariable("roomId") Long roomId) {
         roomService.reserveRoom(roomId);
