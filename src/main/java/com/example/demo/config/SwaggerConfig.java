7474c8e6232d8e2738b263eb8c3bc4c6b9a9785e
armin <armynm005@gmail.com>
2024-09-18 17:25:08 +0330

    feat: add info & server config to swagger

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,20 +1,34 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Room Reservation API")
-                        .version("1.0")
-                        .description("API for managing rooms reservation"));
+    public OpenAPI openAPI() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("Under development...");
+
+        Contact myContact = new Contact();
+        myContact.setName("am2mcu");
+        myContact.setEmail("armynm005@gmail.com");
+
+        Info info = new Info()
+                .title("Room Reservation API")
+                .version("1.0")
+                .description("API for managing room reservation")
+                .contact(myContact);
+
+        return new OpenAPI().info(info).servers(List.of(server));
     }
 }
