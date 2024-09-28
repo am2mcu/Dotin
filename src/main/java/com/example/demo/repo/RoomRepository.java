6ecbfd0841d827d7d28bcbc0273a84b1aaee25f5
armin <armynm005@gmail.com>
2024-09-28 11:47:36 +0330

    feat: add new queries to RoomRepository

--- src/main/java/com/example/demo/repo/RoomRepository.java
+++ src/main/java/com/example/demo/repo/RoomRepository.java
@@ -5,10 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
     @Query("SELECT r FROM Room r WHERE r.roomId = ?1")
     Optional<Room> findRoomById(Long roomId);
+
+    @Query("SELECT r FROM Room r WHERE r.status = Available")
+    List<Room> getAvailableRooms();
+
+    @Query("SELECT r FROM Room r WHERE r.status = Reserved")
+    List<Room> getReservedRooms();
 }
