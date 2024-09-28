51d56060096a48038a1400b8c49c8784d1704160
armin <armynm005@gmail.com>
2024-09-28 10:24:59 +0330

    refactor: change base url to reservations

--- src/main/java/com/example/demo/controller/ReservationController.java
+++ src/main/java/com/example/demo/controller/ReservationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("reservation")
+@RequestMapping("reservations")
 public class ReservationController {
     private final RoomService roomService;
 
