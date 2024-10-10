72dec18c90fd89940ea0cea2b0dbd7deb9465368
armin <armynm005@gmail.com>
2024-10-10 18:22:59 +0330

    feat: add user_role column as an attribute

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -19,6 +19,8 @@ public abstract class User {
     private Long userId;
     private String username, password;
     private String email;
+    @Column(name = "user_role", insertable = false, updatable = false)
+    private String userRole;
 
     public User(String username, String password, String email) {
         this.username = username;
