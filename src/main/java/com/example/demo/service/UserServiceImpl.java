eb49ecc4c4ba1d9b8d64d50fc99ef97e9bd1b0ba
armin <armynm005@gmail.com>
2024-10-10 18:51:44 +0330

    feat: implement UserDetailsService
    handle login through loadUserByUsername method

---
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> {throw new UsernameNotFoundException("Username not found");});
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getUserRole())
+                .build();
+    }
+}
