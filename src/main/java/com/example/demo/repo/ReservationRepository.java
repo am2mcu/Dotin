f598e501e32dcd540ddafcfdf5e6d34854ddff77
armin <armynm005@gmail.com>
2024-10-15 21:41:28 +0330

    feat: add findByUser

--- src/main/java/com/example/demo/repo/ReservationRepository.java
+++ src/main/java/com/example/demo/repo/ReservationRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repo;
 
 import com.example.demo.model.Reservation;
+import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> getByUser(User user);
 }
