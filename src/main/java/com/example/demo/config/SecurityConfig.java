eb6547a816d69156cb6d5675be8ff85a0468d620
armin <armynm005@gmail.com>
2024-10-15 12:34:35 +0330

    feat(securityFilterChain): add session management

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,37 +1,51 @@
 package com.example.demo.config;
 
-import com.example.demo.model.User;
+import com.example.demo.filter.JwtFilter;
 import com.example.demo.repo.UserRepository;
-import com.example.demo.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private final UserServiceImpl userService;
+    private final UserRepository userRepository;
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtFilter jwtFilter;
 
     @Autowired
-    public SecurityConfig(UserServiceImpl userService) {
-        this.userService = userService;
+    @Lazy
+    public SecurityConfig(UserRepository userRepository, AuthenticationProvider authenticationProvider, JwtFilter jwtFilter) {
+        this.userRepository = userRepository;
+        this.authenticationProvider = authenticationProvider;
+        this.jwtFilter = jwtFilter;
     }
 
     @Bean
     UserDetailsService userDetailsService() {
-        return userService;
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
     @Bean
@@ -53,15 +67,35 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests(authorize -> authorize
-                        .antMatchers("/", "/swagger-ui/**").permitAll()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .antMatchers(HttpMethod.GET, "/rooms/**").permitAll()
                         .antMatchers("/rooms/**").hasRole("MANAGER")
                         .antMatchers("/requests/**").hasAnyRole("MANAGER", "RECEPTIONIST")
                         .anyRequest().authenticated())
-                .formLogin();
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+//        configuration.setAllowedMethods(List.of("GET","POST"));
+//        configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//
+//        source.registerCorsConfiguration("/**",configuration);
+//
+//        return source;
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
