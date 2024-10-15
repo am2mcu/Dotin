fecbd559c607e78144b8756bd5be8a235f371695
armin <armynm005@gmail.com>
2024-10-15 12:54:43 +0330

    feat: add user data transfer object
    use in signup/login

---
+++ src/main/java/com/example/demo/dto/RegisterUserDto.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterUserDto {
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
