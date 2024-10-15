9c06900fbdbe06eda66f6c8683de623d78f4e426
armin <armynm005@gmail.com>
2024-10-15 21:17:43 +0330

    fix: change findRequestByUser to findByUser

--- src/main/java/com/example/demo/repo/RequestRepository.java
+++ src/main/java/com/example/demo/repo/RequestRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.repo;
 
 import com.example.demo.model.Request;
+import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface RequestRepository extends JpaRepository<Request, Long> {
+    List<Request> findByUser(User user);
 }
