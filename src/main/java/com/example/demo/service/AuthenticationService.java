9fd49c0751ba208632781c0f3ac2c01f6644e48b
armin <armynm005@gmail.com>
2024-10-15 12:55:55 +0330

    feat: add auth service

---
+++ src/main/java/com/example/demo/service/AuthenticationService.java
@@ -0,0 +1,56 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.LoginUserDto;
+import com.example.demo.dto.RegisterUserDto;
+import com.example.demo.model.Customer;
+import com.example.demo.model.User;
+import com.example.demo.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDto userDto) {
+        User user = new Customer(
+                userDto.getUsername(),
+                userDto.getPassword(),
+                userDto.getEmail(),
+                userDto.getFirstName(),
+                userDto.getLastName()
+        );
+
+        return userRepository.save(user);
+    }
+
+    public User login(LoginUserDto userDto) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userDto.getUsername(),
+                        userDto.getPassword()
+                )
+        );
+
+        return userRepository.findByUsername(userDto.getUsername())
+                .orElseThrow();
+    }
+}
